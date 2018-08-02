d25583316d9c8edfbb31687dd5b0ade8dceef108
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-02 01:04:18 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,12 @@
 // Code Invitation Component Here
+import React from 'react';
+
+class Invitation extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Invitation;
